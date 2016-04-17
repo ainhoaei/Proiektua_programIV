3e0dfd0f4de4007ab0611defd0ed0f9cab794301
jonalonsov <jonalonso@opendeusto.es>
2016-04-17 20:13:42 +0200

    diario eta recordatorio definitibok, .h falta da

--- recordatorio.c
+++ recordatorio.c
@@ -15,26 +15,28 @@ int main(void){
 	int opcion;
     int total;
     total = 0;
+    
     do{
-        printf("Seleccione la accion a realizar:\n1. Apuntar nota\n2. ver notas\n3. Ver notas de dia especifico\n4. Eliminar nota\n5. Atras \n");
+    	avisoRecordatorio();
+        printf("Seleccione la accion a realizar:\n1. Anyadir recordatorio\n2. Eliminar recordatorio\n3. Atras \n");
         scanf("%d",&opcion);
 
         switch(opcion){
-            case 1: apuntarNota();break;
-            case 2: leer();break;
-            case 3: verEspc();break;
-            case 4: break;
-            case 5: break;
+            case 1: recordatorio();break;
+            case 2: break;
+            case 3: break;
             default: printf("Numero erroneo. Introduzca de nuevo.\n"); break;
         }
 
-    }while( opcion != 5 );
+    }while( opcion != 3 );
 
 	return 0;
 }
 
-int verEspc(){
 
+int recordatorio(){
+
+	char recordatorio[MAX_CAR];
 	char str[MAX_FECHA];
     char dia[MAX_FECHA];
     char mes[MAX_FECHA];
@@ -50,7 +52,7 @@ int verEspc(){
     int b=33;
    
     do{
-	    printf ("FECHA (Introduzca los 2 digitos en cada apartado):\nDia: ");
+	    printf ("Añade la FECHA del recordatorio (Introduzca los 2 digitos en cada apartado):\nDia: ");
 	    fgets(str, MAX_FECHA, stdin);
 	    clear_if_needed(str);
 	   
@@ -92,41 +94,20 @@ int verEspc(){
     strcat(fecha, dia);
     strcat(fecha,"/");
     strcat(fecha,anyo);
-    printf("notas del dia %s:\n", fecha);
+    strcat(fecha,": ");
 
 
-    char read[MAX_CAR];
-	FILE* f;
-        char c;
 
-	int num_lines = 0;
-
-	//abrir fichero para lectura
-	f = fopen("nota.txt", "r");
-       
+	printf("Apunte su recordatorio:\n"); 
 	
+	scanf(" %[^\n]s", recordatorio); //honekin espazioak kontuan hartzen ditu!!!
+	strcat(fecha,recordatorio);
+	guardar(fecha);
 
-	while(fgets(read, MAX_CAR, f)) {
-
-		
-
-
-		if(read[0]==fecha[0] && read[1]==fecha[1] && read[3]==fecha[3] && read[4]==fecha[4] && read[6]==fecha[6] && read[7]==fecha[7]){
-
-	    printf("%s", read+10);
-		
-		}
-	 	
-	 }
-
-	 	printf("\n");
-
-	    
-	//cerrar fichero
-	fclose(f);
-	
 
 }
+
+
 
 char* fecha (void)
 {
@@ -145,60 +126,26 @@ char* fecha (void)
   return (char*)buffer;
 }
 
-int guardar(char nuevaNota[])
+int guardar(char nuevoRecordatorio[])
 {
 	
 	FILE* f;
     int c;
     
 	//abrir fichero para escritura "w"
-	f = fopen("nota.txt", "a");
+	f = fopen("recordatorio.txt", "a");
     
 
 	//escribir en fichero un string formateado 	
-	fprintf(f, "========================================\n");
-	//fprintf(f, "Nota del dia: ");
-	fprintf(f, fecha());
 	//fprintf(f, "\n");
 
-	fprintf(f, ": %s\n", nuevaNota);
+	fprintf(f, "%s\n", nuevoRecordatorio);
 		
 	//cerrar fichero
 	fclose(f);
 
 	return 0;
 }
-
-
-
-
-int apuntarNota(){
-
-char str[MAX_CAR];
-char nota[MAX_CAR];
-
-
-
-	printf("Apunte la nota de hoy:\n"); //despues de "hoy" deberia ir la fecha de hoy automaticamente
-	
-	//fflush(stdin);//sin sslush no funciona fgets, no se sabe por que
-	
-	//fgets(str, MAX_CAR, stdin);
-	//clear_if_needed(str);
-	//sscanf(str, "%s", nota);
-	
-
-	//scanf("%s", nota);
-	scanf(" %[^\n]s", nota); //honekin espazioak kontuan hartzen ditu!!!
-
-	guardar(nota);
-
-	return 0;
-
-}
-
-
-
 
 int clear_if_needed (char *str){
 
@@ -210,26 +157,41 @@ int clear_if_needed (char *str){
     return 0;
 }
 
+int avisoRecordatorio(){
 
-int leer()
-{
-	
 	FILE* f;
-        char c;
+	char date[8];
+	strcpy(date,fecha());
+    char read[MAX_CAR];
 
-	int num_lines = 0;
+	int semaforo = 0;
+
+
+	printf("-------------------------\n", date);
+	 printf("Fecha de hoy: %s\n", date);
 
 	//abrir fichero para lectura
-	f = fopen("nota.txt", "r");
+	f = fopen("recordatorio.txt", "r");
        
-	//leer mientras no se llegue al final del fichero EOF
-	while ((c = fgetc(f)) != EOF)
-	{ 
-		putchar(c);
-	}
+	while(fgets(read, MAX_CAR, f)) {
+
+				//strcmp ez zuen funtzionatzen beraz horrela egin dugu
+		if(read[0]==date[0] && read[1]==date[1] && read[3]==date[3] && read[4]==date[4] && read[6]==date[6] && read[7]==date[7]){ 
+
+			//&& read[1]==date[1] && read[3]==date[3] && read[4]==date[4] && read[6]==date[6] && read[7]==date[7]){
+
+	    printf("*%s", read+10);
+	    semaforo++;
+		
+		}
+	 }
+	
+	if(semaforo>0)printf("Tienes %i recordatorio(s)\n", semaforo);
+	if(semaforo==0)printf("No tienes recordatorios\n");
+
+	printf("-------------------------");
 	//cerrar fichero
 	fclose(f);
 
 	return 0;
 }
-
